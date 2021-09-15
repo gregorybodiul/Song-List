e538fb2e56139f52a4517c6e158b4a69aa9bcaaa
Gregory <81109543+gregorybodiul@users.noreply.github.com>
2021-09-15 20:07:12 +0300

    edit scrolling of horizontal list

--- app/src/main/java/com/example/songlist/HorListFragment.java
+++ app/src/main/java/com/example/songlist/HorListFragment.java
@@ -3,12 +3,14 @@ package com.example.songlist;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -34,9 +36,51 @@ public class HorListFragment extends Fragment {
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false);
         recyclerView.setLayoutManager(layoutManager);
 
+        scrolling(recyclerView);
+
         FloatingActionButton floatingActionButton = view.findViewById(R.id.change_list);
         floatingActionButton.setOnClickListener(view1 -> Navigation.findNavController(view1).navigate(R.id.action_horListFragment_to_vertListFragment));
 
         return view;
     }
+
+    private void scrolling(RecyclerView recyclerView) {
+        recyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+            @Override
+            public boolean onInterceptTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
+                if(e.getAction() == MotionEvent.ACTION_UP){
+                    rv.addOnScrollListener(new RecyclerView.OnScrollListener() {
+                        @Override
+                        public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                            super.onScrollStateChanged(recyclerView, newState);
+                            if(newState == 0){
+                                int scrollRange = rv.computeHorizontalScrollRange();
+                                int scrollOffset = rv.computeHorizontalScrollOffset();
+                                int scrollExtent = rv.computeHorizontalScrollExtent();
+
+                                float countSong = (float)scrollRange / (float)scrollExtent;
+                                float scrollItem = (float)scrollRange / (float)scrollOffset;
+                                int itemSelect = Math.round(countSong / scrollItem);
+
+                                recyclerView.removeOnScrollListener(this);
+                                recyclerView.smoothScrollToPosition(itemSelect);
+                            }
+                        }
+                    });
+                }
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
+
+            }
+
+            @Override
+            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+            }
+        });
+    }
+
 }
